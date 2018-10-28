c70a1d73f1ef3ad247a23f4e4b7c09ca0cb35bbb
030 <chocolatey030@gmail.com>
2018-10-28 21:26:38 +0100

    [GH-2] support extraction of values that contain underscores

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
-func scanFile(key string) string {
-	f, err := os.Open("test.yaml")
+func scanFile(file string, key string) string {
+	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,6 +24,7 @@ func scanFile(key string) string {
 	var val string
 
 	for scanner.Scan() {
+		logrus.Debug("Check whether file: ", file, " and line: ", scanner.Text(), " contains key: ", key)
 		if strings.Contains(scanner.Text(), key+":") {
 			val = value(scanner.Text())
 		}
@@ -29,26 +33,41 @@ func scanFile(key string) string {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
 	return val
 }
 
 func value(keyValue string) string {
-	re := regexp.MustCompile("^[a-z]+: (.*)$")
+	re := regexp.MustCompile("^[a-z-_]+: (.*)$")
 	match := re.FindStringSubmatch(keyValue)
+
+	if len(match) == 0 {
+		log.Fatal("Cannot extract value for key, but was: '", keyValue,
+			"' please check whether the regex matches the key")
+	}
+
+	logrus.Debug("MATCH: ", match)
+
 	return match[1]
 }
 
-// func readYAML() string {
-// 	bytes, err := ioutil.ReadFile("test.yaml")
-// 	if err != nil {
-// 		fmt.Print(err)
-// 	}
-
-// 	str := string(bytes)
-
-// 	return str
-// }
-
 func main() {
-	fmt.Println(scanFile("a"))
+	key := flag.String("key", "key", "Specify the key")
+	yamlFile := flag.String("yamlFile", "file.yaml", "Path to a yaml file")
+	debug := flag.Bool("debug", false, "Whether debugging should be enabled")
+
+	flag.Parse()
+
+	if *debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	logrus.Debug("key: ", *key)
+	logrus.Debug("yamlFile: ", *yamlFile)
+
+	value := scanFile(*yamlFile, *key)
+	if value == "" {
+		log.Fatal("File: ", *yamlFile, " does not contain key: ", *key)
+	}
+	fmt.Println(value)
 }
