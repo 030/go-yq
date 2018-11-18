15150235bd3e069ac46df2cd0a4dc0c4083183f9
030 <chocolatey030@gmail.com>
2018-11-18 23:01:52 +0100

    [GH-11] add exe extension to windows binary

--- releases.sh
+++ releases.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 buid_and_checksum() {
-    env GOOS=$1 GOARCH=amd64 go build -o go-yq-$1
-    sha512sum go-yq-$1 > go-yq-${1}.sha512.txt
+    env GOOS=$1 GOARCH=amd64 go build -o go-yq-${1}${2}
+    sha512sum go-yq-${1}${2} > go-yq-${1}${2}.sha512.txt
 }
 
 buid_and_checksum darwin
 buid_and_checksum linux
-buid_and_checksum windows
+buid_and_checksum windows .exe
